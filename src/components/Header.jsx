7e994a862feaa3870fd34f0374e05794d201e1e2
Ushan Niluminda Jayawardana <princeofpersiatwathrones@gmail.com>
2023-05-05 09:48:50 +0300

    fourth commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="w-full flex justify-between items-center bg-light_green py-4 px-16">
-      <Link to="/">
+      <Link to="/DALLE-E/">
         <img
           src={logo}
           alt="logo"
           className="w-28 object-contain cursor-pointer"
         />
       </Link>
-      <Link to="/create-post">
+      <Link to="/DALLE-E/create-post">
         <button className="px-4 py-2 rounded-md bg-dark text-space_white hover:bg-space_white hover:text-dark transition ease-in-out hover:scale-[1.1] border-none cursor-pointer">
           create
         </button>
